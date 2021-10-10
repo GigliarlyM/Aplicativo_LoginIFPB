ad8f4885f2976ecec600044efb87471bbc6c6db8
GigliarlyM <gigliarlymarcelino@gmial.com>
2021-10-09 22:55:48 -0300

    Atualização!

--- app/src/main/java/com/example/sepodevotar/CalculoIdade.java
+++ app/src/main/java/com/example/sepodevotar/CalculoIdade.java
@@ -1,13 +1,21 @@
 package com.example.sepodevotar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class CalculoIdade extends AppCompatActivity {
+
+    ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +23,25 @@ public class CalculoIdade extends AppCompatActivity {
         setContentView(R.layout.activity_calculo_idade);
 
         getSupportActionBar().hide();
-    }
-}
 
-class CalculaIdade {
-    //Calcula a Idade baseado em java.util.Date
-    public static int CalculaIdade(java.util.Date dataNasc) {
+        this.mViewHolder.textResult = findViewById(R.id.text_resultado);
+        EditText textData = findViewById(R.id.text_Data);
+        java.util.Date dataNasc = (Date) textData.getText();
+
+        int idade = calculaIdade(dataNasc);
+
+    }
+
+    public static class ViewHolder{
+        TextView textResult;
+    }
+
+    public static void testVoto(){
+
+    }
+
+    public static int calculaIdade(java.util.Date dataNasc) {
+        //Calcula a Idade baseado em java.util.Date
 
         Calendar dateOfBirth = new GregorianCalendar();
         dateOfBirth.setTime(dataNasc);
@@ -38,10 +59,9 @@ class CalculaIdade {
         //se a data de hoje é antes da data de Nascimento, então diminui 1(um)
 
         if (today.before(dateOfBirth)) {
-
             age--;
-
         }
         return age;
     }
+
 }
