4fc4d1978a757c3acfa1eb93f33c5e3ee96ad50d
GigliarlyM <gigliarlymarcelino@gmial.com>
2021-10-11 14:02:01 -0300

    Melhorias

--- app/src/main/java/com/example/sepodevotar/MainActivity.java
+++ app/src/main/java/com/example/sepodevotar/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    public String nameUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +47,6 @@ public class MainActivity extends AppCompatActivity {
         Button btnConfirm;
         EditText editUser;
     }
-
-    public String nameUser;
 
     private AlertDialog alerta;
 
