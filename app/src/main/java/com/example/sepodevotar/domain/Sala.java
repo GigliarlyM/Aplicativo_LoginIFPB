35155dfcde24146c2c783df2f4d3eae21dd87543
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2024-01-11 10:14:17 -0300

    atualizacao

---
+++ app/src/main/java/com/example/sepodevotar/domain/Sala.java
@@ -0,0 +1,6 @@
+package com.example.sepodevotar.domain;
+
+public class Sala {
+    String name;
+    boolean status;
+}
--- app/src/main/java/com/example/sepodevotar/service/StatusService.java
+++ app/src/main/java/com/example/sepodevotar/service/StatusService.java
@@ -1,6 +1,8 @@
 package com.example.sepodevotar.service;
 
-import com.example.sepodevotar.domain.Repository;
+import com.example.sepodevotar.domain.Sala;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -9,8 +11,8 @@ import retrofit2.http.Path;
 public interface StatusService {
     // Definir o end point para a requesição de busca
     @GET("status")
-    Call<Repository> statusService();
+    Call<List<Sala>> statusService();
 
     @GET("status/{sala}")
-    Call<Repository> statusUnicoService(@Path("sala") String sala);
+    Call<List<Sala>> statusUnicoService(@Path("sala") String sala);
 }
